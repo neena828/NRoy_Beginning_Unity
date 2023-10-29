3c935083bbed364aca47aec3560b316dc7dfd2ef
Neena Roy <neenaroy828@gmail.com>
2023-10-28 22:00:16 -0700

    Hour 11 Part 2

--- NRoy_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ NRoy_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -1,9 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 public class PrefabGenerator : MonoBehaviour
 {
     public GameObject prefab;
+    void Start()
+    {
+        int position = -5;
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(prefab, new UnityEngine.Vector3(position, 0f, -5f), transform.rotation);
+            position++;
+        }
+    }
     void Update()
     {
         // Whenever we hit the B key we will generate a prefab at the
