ac1c555e579baa7b1a3553697bff406355c7e482
Neena Roy <neenaroy828@gmail.com>
2023-11-04 22:02:14 -0700

    Hour 14

---
+++ NRoy_Hour14+16/Assets/Hour 14 Package/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+	public string sceneToLoad;
+
+
+	public void LoadGame ()
+	{
+		SceneManager.LoadScene(sceneToLoad);
+	}
+}
